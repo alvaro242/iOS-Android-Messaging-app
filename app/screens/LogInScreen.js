64709e312a400eff644d3a03207cc3b14cc6b75a
Alvaro Mora <102562035+alvaro242@users.noreply.github.com>
2023-03-10 22:47:49 +0000

    login screen login request to API

--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -1,36 +1,113 @@
 
-import { StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert} from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import axios from "axios";
+
+
+
+const loginValidationSchema = yup.object().shape({
+
+  
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(9, ({ min }) => `Password must be at least ${min} characters`)
+    
+})
+
+
 
 
 export default function LogInScreen({ navigation }) {
   
   return (
+
+    
     
     <View style={styles.container}>
         <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo.png")}/>
-            
+            <Image style={styles.logo} source={require("../assets/logo.png")}/>
         </View>
-
+        
+        <Text>Create a new account</Text>
 
         <View style={styles.formContainer}>
-    
-                <TextInput placeholder="Insert username" style={styles.input} />
-           
-            
-            <TextInput placeholder="Insert username" style={styles.input} />
+            <Formik
+            validationSchema={loginValidationSchema}
+            initialValues={{   email: '', password: ''}}
+            onSubmit={values => 
+                
+                
+                axios.post("http://localhost:3333/api/1.0.0/login/", values)
+                .then ((res) => {
 
+                    console.log(res.data.token);
+                
+                })
+                .catch ((err) =>{
+                    
+                    console.log(err) })
+
+                
+
+                
+            }
+          >
+        
+            {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+              <>
+                
+                <TextInput
+                  name="email"
+                  placeholder="Email Address"
+                  style={styles.input}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="text"
+                />
+                {errors.email &&
+                    <Text style={styles.error}>{errors.email}</Text>
+                }
+                <TextInput
+                  name="password"
+                  placeholder="Password"
+                  autoCapitalize='none'
+                  style={styles.input}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry
+                />
+                {errors.password &&
+                    <Text style={styles.error}>{errors.password}</Text>
+                }
+                
+                <Button onPress={handleSubmit} title="Log In" disabled={!isValid} />
+
+                
+                
+              </>
+            )}
+          </Formik>
         </View>
     </View>
     
   );
 }
 
+
+
 const styles = StyleSheet.create({
   
     container: {
         
         justifyContent: 'center',
+        alignItems: "center",
         backgroundColor: "white",
   },
     logo : {
@@ -45,24 +122,30 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     formContainer : {
-        flex: 1,
-        backgroundColor: '#ebebeb',
-        
-        height: 70,
-        alignItems: 'center',
-        justifyContent: 'center',
+        width: '80%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    elevation: 10,
+   
     },
  
     input : {
 
-        alignItems: 'center',
-        justifyContent: 'center',
-        
-        backgroundColor: 'white',
-        borderWidth: 0.2,
-        borderRadius: 4,
-        paddingHorizontal: 10,
-        marginVertical: 5
+
+    height: 50,
+    padding: 10,
+    width: '90%',
+    margin: 10,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+    },
+
+    error :{
+    fontSize: 14,
+    color: 'red',
     }
   
 });
--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -1,21 +1,184 @@
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const loginValidationSchema = yup.object().shape({
+
+  firstName: yup
+    .string()
+    .required(''), //empty so we dont show any message is is not completed
+  lastName: yup
+    .string()
+    .required(''),
+  email: yup
+    .string()
+    .email(" ")
+    .required(''),
+  password: yup
+    .string()
+    .min(9, ({ min }) => `Password must be at least ${min} characters`)
+    .matches(
+
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{9,})/,
+        "Weak password. Make sure to include upper, lower and special characters"
+      ),
+  confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password')], 'Passwords do not match')
+      .required(''),
+})
+
+
 
 
 export default function SignUpScreen({ navigation }) {
   
   return (
+    
     <View style={styles.container}>
-      <Text>Sign Up Screen!!</Text>
+        <View style={styles.logoContainer}>
+            <Image style={styles.logo} source={require("../assets/logo.png")}/>
+        </View>
+        
+        <Text>Create a new account</Text>
+
+        <View style={styles.formContainer}>
+            <Formik
+            validationSchema={loginValidationSchema}
+            
+            
+            initialValues={{ firstName:'', lastName:'',  email: '', password: '', confirmPassword: ''}}
+            onSubmit={values => 
+                
+                
+                delete values["confirmPassword"] &&
+                console.log(values)
+                
+            }
+          >
+        
+            {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+              <>
+                <TextInput
+                  name="firstName"
+                  placeholder="First Name"
+                  style={styles.input}
+                  onChangeText={handleChange('firstName')}
+                  onBlur={handleBlur('firstName')}
+                  value={values.firstName}
+                  keyboardType="email-address"
+                />
+                {errors.firstName &&
+                    <Text style={styles.error}>{errors.firstName}</Text>
+                }
+                <TextInput
+                  name="lastName"
+                  placeholder="Last Name"
+                  style={styles.input}
+                  onChangeText={handleChange('lastName')}
+                  onBlur={handleBlur('lastName')}
+                  value={values.lastName}
+                  keyboardType="text"
+                />
+                {errors.lastName &&
+                    <Text style={styles.error}>{errors.lastName}</Text>
+                }
+                <TextInput
+                  name="email"
+                  placeholder="Email Address"
+                  autoCapitalize='none'
+                  style={styles.input}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="text"
+                />
+                {errors.email &&
+                    <Text style={styles.error}>{errors.email}</Text>
+                }
+                <TextInput
+                  name="password"
+                  placeholder="Password"
+                  style={styles.input}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry
+                />
+                {errors.password &&
+                    <Text style={styles.error}>{errors.password}</Text>
+                }
+                <TextInput
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  style={styles.input}
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={handleBlur('confirmPassword')}
+                  value={values.confirmPassword}
+                  secureTextEntry
+                />
+                {errors.confirmPassword &&
+                    <Text style={styles.error}>{errors.confirmPassword}</Text>
+                }
+                
+                <Button onPress={handleSubmit} title="Log In" disabled={!isValid} />
+                
+              </>
+            )}
+          </Formik>
+        </View>
     </View>
+    
   );
 }
 
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+    container: {
+        
+        justifyContent: 'center',
+        alignItems: "center",
+        backgroundColor: "white",
   },
+    logo : {
+        width: 60,
+        height: 45 ,
+        margin: 10,
+    
+    }   ,
+    logoContainer : {
+        
+      
+        alignItems: 'center',
+    },
+    formContainer : {
+        width: '80%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    elevation: 10,
+   
+    },
+ 
+    input : {
+
+    height: 50,
+    padding: 10,
+    width: '90%',
+    margin: 10,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+    },
+
+    error :{
+    fontSize: 14,
+    color: 'red',
+    }
+  
 });
